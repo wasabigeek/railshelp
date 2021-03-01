8887c39f0dc89068d3f4f1f69f0b850653b942b8
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-03-01 20:31:44 +0800

    build: Specify correct colors for purgeCss

--- components/Pill.jsx
+++ components/Pill.jsx
@@ -8,7 +8,7 @@ export default function Pill({
   leftIcon,
 }) {
   /** purgecss: border-dashed */
-  /** purgecss: border-green-400 bg-green-100 text-green-800 hover:bg-green-200 text-green-500 */
+  /** purgecss: border-blue-400 bg-blue-100 text-blue-800 hover:bg-blue-200 text-blue-500 */
   /** purgecss: border-yellow-400 bg-yellow-100 text-yellow-800 hover:bg-yellow-200 text-yellow-500 */
   /** purgecss: border-gray-400 bg-gray-100 text-gray-800 hover:bg-gray-200 text-gray-500 */
   return (
