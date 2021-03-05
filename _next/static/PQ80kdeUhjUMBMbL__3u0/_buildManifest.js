1603800d7656eb991107ba2b6e743e585474ff20
wasabigeek <wasabigeek@users.noreply.github.com>
2021-03-05 02:41:22 +0000

    Deploying to gh-pages from  @ 1986ae1e183be61902b053effed9b6d372d9fcd6 🚀

---
+++ _next/static/PQ80kdeUhjUMBMbL__3u0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-f8252d19c221f21df8ff.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5bd9a628a808645ee1cc.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
