abd20495433404f16519415a80e6538f236d18a0
Abhishek <abhishek.ak@bytedance.com>
2021-03-07 09:56:49 +0800

    fix: tests

--- components/FieldInput.test.jsx
+++ components/FieldInput.test.jsx
@@ -15,7 +15,7 @@ it("renders empty inputs when no value is given", async () => {
   expect(selects.length).toBe(2);
 });
 
-it("updates the field type when changed & is not valid", async () => {
+it("updates the field type when changed", async () => {
   const mockHandleUpdate = jest.fn();
   render(<FieldInput value="" onUpdate={mockHandleUpdate} />);
 
@@ -24,14 +24,13 @@ it("updates the field type when changed & is not valid", async () => {
   expect(mockHandleUpdate).toHaveBeenCalledWith(":date");
 });
 
-it("updates the field type when changed & is valid", async () => {
+it("updates the field name when changed", async () => {
   const mockHandleUpdate = jest.fn();
   render(<FieldInput value="" onUpdate={mockHandleUpdate} />);
 
   const fieldTypeSelect = screen.getAllByRole("combobox")[0];
   const fieldName = screen.getByLabelText("Name");
 
-  userEvent.type(fieldName, "created_at");
-  userEvent.selectOptions(fieldTypeSelect, "date");
-  expect(mockHandleUpdate).lastCalledWith("created_at:date");
+  userEvent.type(fieldName, "c");
+  expect(mockHandleUpdate).toHaveBeenCalledWith("c:");
 });
