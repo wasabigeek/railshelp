97ca4eaa253e44d5b0881e2e7b736558a68f2cf0
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-03-24 22:10:48 +0800

    feat: Parse AddColumnsToTable format

--- helpers/parseMigrationFormat.test.js
+++ helpers/parseMigrationFormat.test.js
@@ -1,7 +1,14 @@
 import parseMigrationFormat from './parseMigrationFormat';
 
 describe('parseMigrationFormat()', () => {
-  it('returns empty strings if input is empty string', () => {
+  it('returns Custom Migration if input is empty string', () => {
     expect(parseMigrationFormat('')).toEqual('Custom Migration');
+  });
+  it('returns AddColumnsToTable format correctly', () => {
+    expect(parseMigrationFormat('AddXYZToABC')).toEqual('AddColumnsToTable');
+    expect(parseMigrationFormat('Add123ToABC')).toEqual('AddColumnsToTable');
+    expect(parseMigrationFormat('AddTo')).not.toEqual('AddColumnsToTable');
+    expect(parseMigrationFormat('AddXYZTo')).not.toEqual('AddColumnsToTable');
+    expect(parseMigrationFormat('AddToABC')).not.toEqual('AddColumnsToTable');
   });
 });
