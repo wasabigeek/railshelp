65987a6716bdd9e7ffa9b06dff732984c72b31fe
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-03-27 17:06:33 +0800

    refactor: Improve styling

--- components/MigrationEditor.jsx
+++ components/MigrationEditor.jsx
@@ -16,7 +16,7 @@ const CustomMigrationForm = ({ initialName, onChange }) => {
   };
 
   return (
-    <div>
+    <div className="pt-4">
       <label
         htmlFor="model-name-input"
         className="block font-medium text-gray-700"
@@ -56,7 +56,7 @@ const AddColumnsForm = ({ initialName, onChange }) => {
   };
 
   return (
-    <div>
+    <div className="pt-4">
       <label
         htmlFor="model-name-input"
         className="block font-medium text-gray-700"
@@ -68,7 +68,7 @@ const AddColumnsForm = ({ initialName, onChange }) => {
       <input
         id="model-name-input"
         data-testid="add-columns-name"
-        className="mt-1 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+        className="mt-1 mx-2 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
         placeholder="Columns"
         value={nameParts.columnsName}
         onChange={(e) => handleChange({ columnsName: e.target.value })}
@@ -77,7 +77,7 @@ const AddColumnsForm = ({ initialName, onChange }) => {
       <input
         id="model-name-input"
         data-testid="add-to-table-name"
-        className="mt-1 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
+        className="mt-1 mx-2 border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
         placeholder="Table"
         value={nameParts.tableName}
         onChange={(e) => handleChange({ tableName: e.target.value })}
@@ -95,27 +95,28 @@ const MigrationEditor = ({ initialName = "", onChange }) => {
       <h2 className="text-xl leading-6 font-medium text-gray-900">
         Edit Migration
       </h2>
-      <div className="mt-4">
-        <label
-          className="block font-medium text-gray-700"
-          htmlFor="migration_format"
-        >
-          Format
-        </label>
-        <select
-          id="migration_format"
-          value={format}
-          className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-          onChange={(e) => setFormat(e.target.value)}
-        >
-          <option disabled value={""}>
-            -- required --
-          </option>
-          {Object.values(MIGRATION_FORMATS).map((type) => (
-            <option key={type}>{type}</option>
-          ))}
-        </select>
-
+      <div className="flex flex-col justify-between mt-4 space-y-6 divide-y divide-gray-100">
+        <div>
+          <label
+            className="block font-medium text-gray-700"
+            htmlFor="migration_format"
+          >
+            Format
+          </label>
+          <select
+            id="migration_format"
+            value={format}
+            className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            onChange={(e) => setFormat(e.target.value)}
+          >
+            <option disabled value={""}>
+              -- required --
+            </option>
+            {Object.values(MIGRATION_FORMATS).map((type) => (
+              <option key={type}>{type}</option>
+            ))}
+          </select>
+        </div>
         {format && format == MIGRATION_FORMATS.ADD_COLUMNS ? (
           <AddColumnsForm initialName={initialName} onChange={onChange} />
         ) : (
