241078718a9870697091e22f75afdbc40acbc381
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-06-07 15:57:25 +0800

    Add Google Analytics
    If anyone knows a free and privacy-friendly alternative, please share!

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../helpers/ga'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
