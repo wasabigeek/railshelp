49188ee2bbbd3b4e688d18acbc295d118376cfc0
wasabigeek <wasabigeek@users.noreply.github.com>
2022-07-16 13:30:50 +0800

    Add note for issue with decimal

--- components/PrecisionScaleConfig.jsx
+++ components/PrecisionScaleConfig.jsx
@@ -8,39 +8,51 @@ function PrecisionScaleConfig({ value, onChange }) {
   };
 
   return (
-    <div className="flex w-full justify-between space-x-2 mt-2">
-      <div className="flex-1">
-        <label
-          className="text-sm font-normal text-gray-700"
-          htmlFor="precision_input"
-        >
-          Precision
-        </label>
-        <input
-          id="precision_input"
-          type="number"
-          value={precision}
-          className="text-input"
-          onChange={(e) =>
-            updatePrecisionAndScale({ precision: e.target.value })
-          }
-        />
+    <div>
+      <div className="flex w-full justify-between space-x-2 mt-2">
+        <div className="flex-1">
+          <label
+            className="text-sm font-normal text-gray-700"
+            htmlFor="precision_input"
+          >
+            Precision
+          </label>
+          <input
+            id="precision_input"
+            type="number"
+            value={precision}
+            className="text-input"
+            onChange={(e) =>
+              updatePrecisionAndScale({ precision: e.target.value })
+            }
+          />
+        </div>
+        <div className="flex-1">
+          <label
+            className="text-sm font-normal text-gray-700"
+            htmlFor="scale_input"
+          >
+            Scale
+          </label>
+          <input
+            id="scale_input"
+            type="number"
+            value={scale}
+            className="text-input"
+            onChange={(e) => updatePrecisionAndScale({ scale: e.target.value })}
+          />
+        </div>
       </div>
-      <div className="flex-1">
-        <label
-          className="text-sm font-normal text-gray-700"
-          htmlFor="scale_input"
+      <p className="text-sm text-gray-500 mt-2">
+        Note: depending on your shell, you may need to wrap this in quotes, see&nbsp;
+        <a
+          href="https://github.com/rails/rails/issues/13520#issuecomment-44698117"
+          target="_blank"
+          className="underline hover:text-gray-700"
         >
-          Scale
-        </label>
-        <input
-          id="scale_input"
-          type="number"
-          value={scale}
-          className="text-input"
-          onChange={(e) => updatePrecisionAndScale({ scale: e.target.value })}
-        />
-      </div>
+          issue
+        </a>.
+      </p>
     </div>
   );
 }
